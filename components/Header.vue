98ca794f4b16f33a392b2a33ca1b33f63d064630
Daniel <52977970+dbueker@users.noreply.github.com>
2023-08-27 21:41:37 +0200

    Header top padding

--- components/Header.vue
+++ components/Header.vue
@@ -6,7 +6,7 @@ const props = defineProps({
 </script>
 
 <template>
-    <div class="w-full pt-32 lg:pt-40 mb-24 md:mb-28 lg:mb-32">
+    <div class="w-full pt-20 mb-24 md:mb-28 lg:mb-32">
         <div class="w-full px-6 mx-auto space-y-10 lg:max-w-screen-xl mb-20 text-lightmode-on-background dark:text-darkmode-on-background">
           <p class="text-chapter  ">{{ subtitle }}</p>
           <p class="text-title-mobile lg:text-title-desktop">{{ title }}</p>
