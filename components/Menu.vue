9e6702f9f8a060de9ffe329c8db5bbded3e8f8cc
Daniel <52977970+dbueker@users.noreply.github.com>
2023-08-27 17:45:51 +0200

    Contact Button text

--- components/Menu.vue
+++ components/Menu.vue
@@ -6,6 +6,6 @@ import ButtonSmall from './ButtonSmall.vue';
     <div class="px-6 pt-6 space-x-6 flex justify-end items-center">
         <NuxtLink v-if="$route.path === '/'" to="/about"><p class="text-button text-lightmode-on-background dark:text-darkmode-on-background hover:opacity-70 transition ease-in-out duration-200">About</p></NuxtLink>
         <NuxtLink v-else to="/"><p class="text-button text-lightmode-on-background dark:text-darkmode-on-background hover:opacity-70 transition ease-in-out duration-200">Home</p></NuxtLink>
-        <a href="mailto:da.bueker@gmail.com"><ButtonSmall text="Say hi" /></a>
+        <a href="mailto:da.bueker@gmail.com"><ButtonSmall text="Say hello" /></a>
     </div>
 </template>
