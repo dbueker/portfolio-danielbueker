e85549bdcb3bee65c6f7466feba652b591f0b373
Daniel <52977970+dbueker@users.noreply.github.com>
2023-08-27 17:43:11 +0200

    Button Border
    Border style adjustments to dark and lightmode

--- components/ButtonSmall.vue
+++ components/ButtonSmall.vue
@@ -3,5 +3,5 @@
 </script>
 
 <template>
-    <button class="rounded-full min-w-[80px] py-2 px-6 border border-lightmode-primary dark:border-darkmode-primary text-body text-lightmode-on-background dark:text-darkmode-on-background lg:hover:bg-accent lg:hover:text-on-accent lg:hover:border-accent transition ease-out delay-75 duration-200"> {{ text }}</button>
+    <button class="rounded-full min-w-[80px] py-2 px-6 border dark:border-2 border-lightmode-primary dark:border-darkmode-primary text-button text-lightmode-on-background dark:text-darkmode-on-background lg:hover:bg-accent lg:hover:text-on-accent lg:hover:border-accent transition ease-out delay-75 duration-200"> {{ text }}</button>
 </template>
