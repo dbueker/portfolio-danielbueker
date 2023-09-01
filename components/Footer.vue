9c39c623a7e59b61d5620661461d9f600a2ec04e
Daniel <52977970+dbueker@users.noreply.github.com>
2023-09-01 09:43:26 +0200

    Update Footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -4,8 +4,8 @@
           <div class="space-y-6 mb-24 md:mb-28 lg:mb-32 max-w-xl text-lightmode-on-primary dark:text-darkmode-on-primary">
             <a href="mailto:da.bueker@gmail.com" class="text-title-mobile lg:text-title-desktop">Say hello</a>
             <p class="text-body pb-6">I love to team up for improving or designing new things from scratch. If you are curious about what we could do together, say hello!</p>
-            <a class="text-body-bold underline hover:opacity-60 transition ease-in-out duration-200" href="mailto:da.bueker@gmail.com" >da.bueker@gmail.com</a>
-            <a class="text-body-bold underline hover:opacity-60 transition ease-in-out duration-200" href="https://www.linkedin.com/in/danielbueker/" target="_blank">LinkedIn</a>
+            <div><a class="text-body-bold underline hover:opacity-60 transition ease-in-out duration-200" href="mailto:da.bueker@gmail.com" >da.bueker@gmail.com</a></div>
+            <div><a class="text-body-bold underline hover:opacity-60 transition ease-in-out duration-200" href="https://www.linkedin.com/in/danielbueker/" target="_blank">LinkedIn</a></div>
           </div>
           <div class="flex flex-row space-x-4 max-w-xl text-body-small  text-lightmode-on-primary-subdued dark:text-darkmode-on-primary-subdued">
               <p>© 2023 Daniel Büker</p>
