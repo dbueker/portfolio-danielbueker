f238984980045211d68978a38db9c05f104204b8
Daniel <52977970+dbueker@users.noreply.github.com>
2023-09-01 16:28:16 +0200

    Intro bottom margin

--- components/Intro.vue
+++ components/Intro.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="pt-20 min-h-[60vh]">
-        <div class="text-lightmode-on-background dark:text-darkmode-on-background pb-12 md:pb-20 lg:max-w-[800px]">
+    <div class="pt-20 mb-20 md:mb-28 lg:mb-40">
+        <div class="text-lightmode-on-background dark:text-darkmode-on-background lg:max-w-[800px]">
             <p class="text-subtitle-mobile lg:text-subtitle-desktop">Daniel Büker</p>
             <p class="text-title-mobile lg:text-title-desktop">Digital Product Designer</p>
             <p class="pt-20 text-body-semibold text-lightmode-on-background dark:text-darkmode-on-background">I build high quality user experiences with a keen eye for detail and solid technical expertise.</p>
