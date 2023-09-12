ac9891eb57a7f8ed63f545ccb8d1c75dc4bc1488
Daniel <52977970+dbueker@users.noreply.github.com>
2023-09-12 17:10:55 +0200

    Skillset text

--- components/Skillset.vue
+++ components/Skillset.vue
@@ -6,7 +6,7 @@ import TextTeaser from './TextTeaser.vue'
 <template>
     <div class="bg-lightmode-surface dark:bg-darkmode-surface px-6 py-20">
         <div class="space-y-8 md:space-y-0 md:grid md:grid-cols-4 md:gap-20 lg:mx-auto lg:max-w-screen-xl">
-            <p class="text-chapter text-lightmode-on-surface dark:text-darkmode-on-surface">I have a focus on</p>
+            <p class="text-chapter text-lightmode-on-surface dark:text-darkmode-on-surface">I can help you with</p>
             <div class="space-y-10 md:col-span-3">
                 <div class="space-y-10">
                 <TextTeaser headline="Experience Design" text="With a strong foundation in psychology I conduct and build on research to create thoughtful concepts and a holistic user experience."/>
