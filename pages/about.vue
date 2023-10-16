bc699796898865493fd3032688a93d62f7172a25
Daniel <52977970+dbueker@users.noreply.github.com>
2023-10-16 11:32:03 +0200

    Text update
    Intro and About

--- pages/about.vue
+++ pages/about.vue
@@ -7,7 +7,7 @@ import Portrait from '../assets/Portrait.jpg';
         <div class="px-6 mx-auto lg:max-w-screen-xl grid grid-cols-1 md:grid-cols-2 gap-y-20 md:gap-x-10 place-items-center place-content-center mb-24 md:mb-28 lg:mb-32 pt-20 text-lightmode-on-background dark:text-darkmode-on-background">
           <div class="space-y-6">
             <p class="text-h1-mobile lg:text-h1-desktop">I'm Daniel 👋</p>
-            <p class="text-body ">I’m a Digital Product Designer and currently live in the wonderful city of Hamburg. I strive to make our life as networked humans easier by creating digital services that are as simple, reliable and fun as possible. If you have problems to be solved or just want to chat, feel free to contact me!</p>
+            <p class="text-body ">I like to design everything that requires human interaction. I strive to make our life easier by creating interfaces and experiences that are as simple, reliable and fun as possible. If you have problems to be solved or just want to chat, feel free to <a class="text-body-bold underline md:hover:opacity-60 transition ease-in-out duration-200" href="https://www.linkedin.com/in/danielbueker/" target="_blank">contact me</a><span class="text-body-bold">!</span></p>
           </div>
           <img :src=Portrait alt="Portrait of Daniel" class="rounded w-full md:w-fit md:max-h-[640px]" />
         </div>
