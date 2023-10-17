4891390ce6539d6415177d9996da7cc27e1aa343
Daniel <52977970+dbueker@users.noreply.github.com>
2023-10-17 16:59:26 +0200

    Button Sizing

--- components/ButtonBackToTop.vue
+++ components/ButtonBackToTop.vue
@@ -10,5 +10,5 @@ function scrollToTop() {
 </script>
 
 <template>
-    <button @click="scrollToTop" class="flex rounded-full min-w-[80px] py-2 px-6 space-x-2 text-button border-2 border-lightmode-on-surface-variant dark:border-darkmode-on-surface-variant text-lightmode-on-surface-variant dark:text-darkmode-on-surface-variant md:hover:scale-[104%] md:active:scale-[102%] transition ease-in-out duration-200"><p>Back to top</p><img :src=ArrowTop alt="Arrow facing upwards"></button>
+    <button @click="scrollToTop" class="flex rounded-full min-w-[80px] py-4 px-6 space-x-2 text-button border-2 border-lightmode-on-surface-variant-subdued dark:border-darkmode-on-surface-variant-subdued text-lightmode-on-surface-variant dark:text-darkmode-on-surface-variant md:hover:scale-[104%] md:active:scale-[102%] transition ease-in-out duration-200"><p>Back to top</p><img :src=ArrowTop alt="Arrow facing upwards"></button>
 </template>
