2ed52371b5d90d86520a265234f4ab794c99bf22
Daniel <52977970+dbueker@users.noreply.github.com>
2024-02-10 12:39:06 +0100

    Intro spacing

--- components/Intro.vue
+++ components/Intro.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="px-6 pt-20 mb-20 md:mb-28 lg:mb-40">
-        <div class="text-lightmode-on-background dark:text-darkmode-on-background lg:max-w-[800px]">
+        <div class="text-lightmode-on-background dark:text-darkmode-on-background lg:max-w-[900px]">
             <p class="text-subtitle-mobile lg:text-subtitle-desktop">Daniel Büker</p>
             <p class="text-title-mobile lg:text-title-desktop">Digital Product Designer</p>
-            <p class="pt-20 text-body text-lightmode-on-background dark:text-darkmode-on-background">I build high quality user experiences and human interfaces with a keen eye for detail and solid technical expertise, currently at <a class="text-body-bold md:hover:opacity-70 transition ease-in-out duration-150 text-lightmode-primary dark:text-darkmode-primary" href="https://mobimeo.com/" target="_blank">Mobimeo</a>.</p>
+            <p class="pt-12 text-body text-lightmode-on-background dark:text-darkmode-on-background">I build high quality user experiences and human interfaces with a keen eye for detail and solid technical expertise, currently at <a class="text-body-bold md:hover:opacity-70 transition ease-in-out duration-150 text-lightmode-primary dark:text-darkmode-primary" href="https://mobimeo.com/" target="_blank">Mobimeo</a>.</p>
         </div>
     </div>
 </template>
