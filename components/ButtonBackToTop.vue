061848377ce887ae4b86b10070bb839509c22751
Daniel <52977970+dbueker@users.noreply.github.com>
2026-01-26 22:21:29 +0100

    back to top fix

--- components/ButtonBackToTop.vue
+++ components/ButtonBackToTop.vue
@@ -10,5 +10,5 @@ function scrollToTop() {
 </script>
 
 <template>
-    <button @click="scrollToTop" class="flex rounded-full py-4 px-6 space-x-2 text-button border-2 border-on-surface-variant-subdued text-on-surface-variant md:hover:scale-[104%] md:active:scale-[102%] transition ease-in-out duration-200"><span>Scroll to top</span><img :src=ArrowTop alt="Arrow facing upwards"></button>
+    <button @click="scrollToTop" class="flex whitespace-nowrap rounded-full py-4 px-6 space-x-2 text-button border-2 border-on-surface-variant-subdued text-on-surface-variant md:hover:scale-[104%] md:active:scale-[102%] transition ease-in-out duration-200"><span>Scroll to top</span><img :src=ArrowTop alt="Arrow facing upwards"></button>
 </template>
