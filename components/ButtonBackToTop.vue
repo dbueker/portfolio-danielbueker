13417210a6b9450cef0e7ca5a089f05d0c3cf9d2
Daniel <52977970+dbueker@users.noreply.github.com>
2026-01-31 21:06:56 +0100

    flex wrap for footer button

--- components/ButtonBackToTop.vue
+++ components/ButtonBackToTop.vue
@@ -9,5 +9,5 @@ function scrollToTop() {
 </script>
 
 <template>
-    <button @click="scrollToTop" class="whitespace-nowrap min-w-[80px] py-4 px-6 text-button text-on-surface-variant md:hover:text-on-surface-variant/70 md:active:scale-[96%] transition ease-in-out duration-200">Scroll to top</button>
+    <button @click="scrollToTop" class="whitespace-nowrap min-w-[80px] py-4 text-button text-on-surface-variant md:hover:text-on-surface-variant/70 md:active:scale-[96%] transition ease-in-out duration-200">Scroll to top</button>
 </template>
